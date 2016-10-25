710697e1ed2e603a3179544b6884206abf698d70
Anthony JODIN <ajodin@e2r1p3.42.fr>
2016-10-25 11:35:54 +0200

    Add some pictures

--- js/controller.js
+++ js/controller.js
@@ -1,7 +1,7 @@
 angular.module('Arcadia', ['ui.bootstrap', 'ngAside'])
 
 .controller("mainCtrl", function($scope, $timeout, $aside, $http) {
-    var totalPictures = 57;
+    var totalPictures = 61;
     $scope.login = "Monsieur";
 	$scope.callImgs = function() {
 		var nbr = Math.floor((Math.random() * totalPictures) + 1);
